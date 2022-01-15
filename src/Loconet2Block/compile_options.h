d2ecd012fbd4c7d7ae575d111021f64592bbb9fc
nh-finescale <77726489+nh-finescale@users.noreply.github.com>
2022-01-15 16:10:58 +0100

    Add files via upload

---
+++ src/Loconet2Block/compile_options.h
@@ -0,0 +1,28 @@
+
+#pragma once
+
+//##########################################################################
+//#
+//#		compile_options.h
+//#
+//#	This file contains compile options:
+//#		-	DEBUGGING_PRINTOUT
+//#			If defined, debug info and/or text is shown on an
+//#			OLED display
+//#
+//#		-	PLATINE_VERSION
+//#			defines for which board version the software will be compiled.
+//#			(differences in the I/O assignment)
+//#
+//##########################################################################
+
+
+//==========================================================================
+//
+//		C O D E   S E L E C T I O N   D E F I N I T I O N S
+//
+//==========================================================================
+
+#define DEBUGGING_PRINTOUT
+
+#define PLATINE_VERSION			5
